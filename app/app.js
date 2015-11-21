92c65f10b44a5d41dbd8ac0a293a6fb2a46de4cc
danielcossins11 <daniel.cossins@yahoo.com>
2015-11-21 16:45:42 -0600

    added more routing

--- app/app.js
+++ app/app.js
@@ -7,6 +7,30 @@ app.config(['$routeProvider',
         templateUrl: 'partials/main.html',
         controller: 'MainCtrl'
       })
+      .when('/planets', {
+        templateUrl: 'partials/planets.html',
+        controller: 'PlanetsCtrl'
+      })
+      .when('/spaceships', {
+        templateUrl: 'partials/spaceships.html',
+        controller: 'SpaceshipsCtrl'
+      })
+      .when('/vehicles', {
+        templateUrl: 'partials/vehicles.html',
+        controller: 'VehiclesCtrl'
+      })
+      .when('/people', {
+        templateUrl: 'partials/people.html',
+        controller: 'PeopleCtrl'
+      })
+      .when('/films', {
+        templateUrl: 'partials/films.html',
+        controller: 'FilmCtrl'
+      })
+      .when('/species', {
+        templateUrl: 'partials/species.html',
+        controller: 'SpeciesCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
