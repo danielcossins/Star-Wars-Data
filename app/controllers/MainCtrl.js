04d397f234dedeca9cff1547cd8617a8541055e2
danielcossins11 <daniel.cossins@yahoo.com>
2015-11-20 23:39:52 -0600

    printing data from api to the dom

--- app/controllers/MainCtrl.js
+++ app/controllers/MainCtrl.js
@@ -2,6 +2,13 @@ app.controller("MainCtrl",
   ["$scope", "$http", "$location",
   function($scope, $http, $location) {
     console.log($location.path());
-    
+    $scope.stuff = [];
+
+
+    $http.get("http://swapi.co/api/people/1/")
+    .then(function(data){
+      console.log(data);
+      $scope.stuff.push(data.data);
+    });
   }
 ]);
