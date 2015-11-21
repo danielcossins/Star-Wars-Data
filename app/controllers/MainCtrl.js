935a339362a7d070b71f78cb7505183a02bc3f1b
danielcossins11 <daniel.cossins@yahoo.com>
2015-11-21 16:25:49 -0600

    added styled links

--- app/controllers/MainCtrl.js
+++ app/controllers/MainCtrl.js
@@ -4,39 +4,12 @@ app.controller("MainCtrl",
     console.log($location.path());
     $scope.stuff = [];
 
-    // var count = 1;
-    // for(var i=0; i<10; i++){
-    //   $http.get("http://swapi.co/api/people/" + count)
-    //   .then(function(data){
-    //     console.log(data);
-    //     $scope.stuff.push(data.data);
-    //   });
-    //   count++;
-    // }
-    var promise = GetAPI("people", 2);
-    promise.then(function(data){
-      $scope.stuff = data;
-    });
+    // var promise = GetAPI("people", 2);
+    // promise.then(function(data){
+    //   $scope.stuff = data;
+    // });
 
 
 
-    // THIS WORKS
-    // var type="people";
-    // var count = 1;
-    // var numOfCalls = 5;
-    // Get();
-
-    // function Get(){
-    //   if(count>numOfCalls){
-    //     return;
-    //   }
-    //   $http.get("http://swapi.co/api/" + type + "/" + count)
-    //   .then(function(data){
-    //     console.log(data);
-    //     $scope.stuff.push(data.data);
-    //     count++;
-    //     Get();
-    //   });
-    // }
   }
 ]);
