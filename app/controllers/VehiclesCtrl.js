c50d40b53a119a7c77dbe3308ae6ca50d50b7b0c
danielcossins11 <daniel.cossins@yahoo.com>
2015-11-22 16:08:24 -0600

    added functionality for all links

--- app/controllers/VehiclesCtrl.js
+++ app/controllers/VehiclesCtrl.js
@@ -1,15 +1,47 @@
 app.controller("VehiclesCtrl", 
-  ["$scope", "$http", "$location", "GetAPI",
-  function($scope, $http, $location, GetAPI) {
+  ["$scope", "$http", "$location", "$window", "GetAPI", "GetOneData",
+  function($scope, $http, $location, $window, GetAPI, GetOneData) {
     console.log($location.path());
     $scope.stuff = [];
+    var type = $location.path();
+    type = type.substr(0, 0) + '' + type.substr(0 + 1);
+    console.log(type);
 
-    // var promise = GetAPI("people", 2);
-    // promise.then(function(data){
-    //   $scope.stuff = data;
-    // });
+    var numOfCalls = 5;
+    var count = 1;
+    Get();
 
+    function Get(){
+      if(count>numOfCalls){
+        return;
+      }
+      $http.get("http://swapi.co/api/" + type + "/" + count)
+      .then(function(data){
+        console.log(data);
+        count++;
+        $scope.stuff.push(data.data);
+        Get();
+      },
+      function(error) {
+        console.log(error);
+        count++;
+        Get();
+      });
+    }
+    
 
+    angular.element($window).bind("scroll", function() {
+      var windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
+      var body = document.body, html = document.documentElement;
+      var docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight,  html.scrollHeight, html.offsetHeight);
+      windowBottom = windowHeight + window.pageYOffset;
+      if (windowBottom >= docHeight) {
+          console.log('bottom reached');
+          GetOneData(type, count).then(function(data){
+            $scope.stuff.push(data);
+          });
+      }
+    });
 
   }
 ]);
